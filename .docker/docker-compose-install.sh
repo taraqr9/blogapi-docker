e837a7f4c0454feea1536cf5041148aa0ae7bf0a
Jatri Taraq <130284178+taraqjatri@users.noreply.github.com>
2023-06-15 17:36:49 +0600

    Create docker-compose-install.sh

---
+++ .docker/docker-compose-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+yes | sudo usermod -aG docker $USER
+
+docker-compose --version
